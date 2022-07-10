0b8e31ac888d900a283c1c28792d98cdcfdb182d
Yuto Watanabe <yuto.w51942@gmail.com>
2022-07-10 17:07:43 +0900

    change header type

--- ch/headers/headers.go
+++ ch/headers/headers.go
@@ -3,28 +3,28 @@ package headers
 const (
 	// User agent client hints
 	// ref. https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers#user_agent_client_hints
-	SecChUa                = "Sec-CH-UA"
-	SecChUaArch            = "Sec-CH-UA-Arch"
-	SecChUaBitness         = "Sec-CH-UA-Bitness"
-	SecChUaFullVersion     = "Sec-CH-UA-Full-Version"
-	SecChUaFullVersionList = "Sec-CH-UA-Full-Version-List"
-	SecChUaMobile          = "Sec-CH-UA-Mobile"
-	SecChUaModel           = "Sec-CH-UA-Model"
-	SecChUaPlatform        = "Sec-CH-UA-Platform"
-	SecChUaPlatformVarsion = "Sec-CH-UA-Platform-Version"
+	SecChUa                = "Sec-Ch-Ua"
+	SecChUaArch            = "Sec-Ch-Ua-Arch"
+	SecChUaBitness         = "Sec-Ch-Ua-Bitness"
+	SecChUaFullVersion     = "Sec-Ch-Ua-Full-Version"
+	SecChUaFullVersionList = "Sec-Ch-Ua-Full-Version-List"
+	SecChUaMobile          = "Sec-Ch-Ua-Mobile"
+	SecChUaModel           = "Sec-Ch-Ua-Model"
+	SecChUaPlatform        = "Sec-Ch-Ua-Platform"
+	SecChUaPlatformVarsion = "Sec-Ch-Ua-Platform-Version"
 
 	// (Deprecated) Device client hints
 	// ref. https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers#device_client_hints
-	ContentDPR    = "Content-DPR"
+	ContentDPR    = "Content-Dpr"
 	DeviceMemory  = "Device-Memory"
-	DPR           = "DPR"
+	DPR           = "Dpr"
 	ViewportWidth = "Viewport-Width"
 	Width         = "Width"
 
 	// Network client hints
 	// ref. https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers#network_client_hints
 	Downlink = "Downlink"
-	ECT      = "ECT"
-	RTT      = "RTT"
+	ECT      = "Ect"
+	RTT      = "Rtt"
 	SaveData = "Save-Data"
 )
